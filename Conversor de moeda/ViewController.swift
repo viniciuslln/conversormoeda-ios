38690202589c0050fbfcf3f11e3b879e0c0b5113
Vinicius Nunes <viniciuslln@outlook.com>
2019-12-22 14:47:50 -0300

    Initial Commit

---
+++ Conversor de moeda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Conversor de moeda
+//
+//  Created by vinicius dev on 22/12/19.
+//  Copyright © 2019 vinicius dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Conversor de moedaTests/Conversor_de_moedaTests.swift
@@ -0,0 +1,34 @@
+//
+//  Conversor_de_moedaTests.swift
+//  Conversor de moedaTests
+//
+//  Created by vinicius dev on 22/12/19.
+//  Copyright © 2019 vinicius dev. All rights reserved.
+//
+
+import XCTest
+@testable import Conversor_de_moeda
+
+class Conversor_de_moedaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
